e600c3e304def73f4548032e440a9dc787ad58d5
Igor Samanchuk <isamanchuk@gmail.com>
2015-06-28 18:16:15 +0300

    test

--- src/week2/homework/MyString.java
+++ src/week2/homework/MyString.java
@@ -1,8 +1,31 @@
 package week2.homework;
 
+import java.util.Scanner;
+
 /**
  * Created by Ihor Samanchuk on 28.06.2015.
  */
 public class MyString {
+    private String incomeString;
+    Scanner scan = new Scanner(System.in);
 
+    public void printString() {
+        System.out.println(incomeString);
+    }
+
+    public void getIncomeString() {
+        System.out.println("Input String, please");
+        incomeString = scan.nextLine();
+    }
+
+    public void stringToChar() {
+        char[] arrayResult = incomeString.toCharArray();
+    }
+
+    public void concatanateSrings() {
+        String resultString = scan.nextLine();
+        System.out.println(resultString.toUpperCase());
+    }
 }
+
+
---
+++ src/week2/homework/TestMyString.java
@@ -0,0 +1,15 @@
+package week2.homework;
+
+import java.util.Scanner;
+
+/**
+ * Created by Ihor Samanchuk on 28.06.2015.
+ */
+public class TestMyString {
+    public static void main(String[] args) {
+        MyString myString = new MyString();
+       // myString.getIncomeString();
+       // myString.stringToChar();
+        myString.concatanateSrings();
+    }
+}
